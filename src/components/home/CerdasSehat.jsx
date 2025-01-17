1e521af3db57fad4610206d78c306b6bdb634941
fiqrioemry <foemry@gmail.com>
2025-01-17 17:25:04 +0700

    update changes

--- src/components/home/CerdasSehat.jsx
+++ src/components/home/CerdasSehat.jsx
@@ -11,18 +11,20 @@ const CerdasSehat = () => {
         <CerdasSehatCard title="terkoneksi" path="/news/terkoneksi" />
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:mb-0 mb-6 py-0 md:py-6">
-        <div className="hidden md:flex items-end col-start-2 md:col-start-1">
+        <CerdasSehatCard title="sejahtera" path="/news/sejahtera" />
+        <CerdasSehatCard title="sehat" path="/news/sehat" />
+        <div className="md:hidden flex items-end col-start-2 md:col-start-1">
           <Link to="/news" className="w-full">
             <div className="py-2 px-4 border text-background text-white border-background">
               Visit News
             </div>
           </Link>
         </div>
-        <CerdasSehatCard title="sejahtera" path="/news/sejahtera" />
-        <CerdasSehatCard title="sehat" path="/news/sehat" />
-        <div className="md:hidden flex items-end col-start-2 md:col-start-1">
-          <Link to="/news" className="w-full">
-            <div className="py-2 px-4 border text-background text-white border-background">
+      </div>
+      <div>
+        <div className="hidden md:grid grid-cols-4 gap-6 ">
+          <Link to="/news" className="col-start-4 col-end-5">
+            <div className="py-2 px-4 border text-background text-white border-background flex justify-center ">
               Visit News
             </div>
           </Link>
